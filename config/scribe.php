12e0e119043a31869110d2b9fefefe13fec27814
Anas Badru Salam <anasbadrusalam@gmail.com>
2025-05-12 14:14:46 +0700

    add enable to scibe config

--- config/scribe.php
+++ config/scribe.php
@@ -8,6 +8,8 @@ use function Knuckles\Scribe\Config\{removeStrategies, configureStrategy};
 // Only the most common configs are shown. See the https://scribe.knuckles.wtf/laravel/reference/config for all.
 
 return [
+    'enabled' => env('SCRIBE_ENABLED', !app()->environment('production')),
+
     // The HTML <title> for the generated documentation.
     'title' => config('app.name') . ' API Documentation',
 
